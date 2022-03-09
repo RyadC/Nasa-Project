f060d0969e9a0fe55785500654a53a74bf76c528
Ryad Chair <r.chair@hotmail.fr>
2022-03-09 14:31:11 +0100

    Ajout du paquet morgan pour gérer les logs

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 // SETTINGS REQUIRE
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 
 // SETTINGS
 const app = express();
@@ -9,13 +11,23 @@ const app = express();
 const planetsRouter = require('./routes/planets/planets.routes');
 
 
-// Pour mettre l'en tete access-control-allowed-origin à toutes mes réponses qui vont sortir du serveur pour que le navigateur ne bloque pas la réponse au client
+// iddleware de sécurité: Pour mettre l'en tete access-control-allowed-origin à toutes mes réponses qui vont sortir du serveur pour que le navigateur ne bloque pas la réponse au client
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
+// Middleware de suivi: Enregistre et log les requetes effectuées à notre serveur
+app.use(morgan('combined'));
+
 // Pour n'accepter que les requetes dont le Content-Type est en json puis le transformer en objet JS
 app.use(express.json());
+
+// Pour servir les actifs de l'application
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.use(planetsRouter);
 
--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,7 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 // On utilise le PORT mis en place par l'administrateur du serveur si cela a été fait sinon on définit sur 8000 (autre que 3000 car 3000 est utilisé par le côté client)
 const PORT = process.env.PORT || 8000;
+// console.log(process.env)
 
 // On préfère utiliser le module hhtp de Node plutot que d'utiliser directement express, puis on passe comme argument qui est l'écouteur de requête l'app de express qui va écouter les requetes et les gérer. Ainsi on sépare la partie serveur et configuration du serveur et la gestion des requetes (qui sera dans un fichier a part entière: app.js)
 const server = http.createServer(app);
